31cefb631e268cd435991b72382348452c664be0
Jaroslav28 <jaroslav030305@gmail.com>
2021-09-21 13:56:31 +0300

    add tuples.py

---
+++ tuples_sets.py
@@ -0,0 +1,14 @@
+#Tuples
+
+#()
+ my_tuple = ('sportot','ēst','stradāt')
+
+ print(len(my_tuple))
+
+ print(my_tuple[1])
+
+ print(my_tuple.index(sportot))
+
+ my_tuple = (11,11,12,11,13,11)
+
+ print(my_tuple.count(11))
