48bff1a6fd291a6434923d67f07ef91e959de5a2
Jaroslav28 <jaroslav030305@gmail.com>
2021-10-04 15:15:21 +0300

    Yaroslav

---
+++ text.py
@@ -0,0 +1,8 @@
+'''
+Jaroslav Pļašečniks
+04.10.2021.
+'''
+
+def Galvena():
+    print('Argument')
+
--- tuples_sets.py
+++ tuples_sets.py
@@ -1,14 +1,9 @@
 #Tuples
 
 #()
- my_tuple = ("sportot","ēst","stradāt")
-
- print(len(my_tuple))
-
- print(my_tuple[1])
-
- print(my_tuple.index(sportot))
-
- my_tuple = (11,11,12,11,13,11)
-
- print(my_tuple.count(11))
+my_tuple = ("sportot","ēst","stradāt")
+print(len(my_tuple))
+print(my_tuple[1])
+print(my_tuple.index('sportot'))  
+my_tuple = (11,11,12,11,13,11)
+print(my_tuple.count(11))
