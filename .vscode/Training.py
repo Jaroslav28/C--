c7022f3ca933247c65eb379da6a85371bf6019b3
Jaroslav28 <jaroslav030305@gmail.com>
2021-10-05 13:34:35 +0300

    Training

---
+++ .vscode/Training.py
@@ -0,0 +1,19 @@
+mish = "jivotnoe"
+if mish== "jivotnoe":
+    print ("Hi")
+
+kompot = "jidkost"
+if kompot== "jidkost":
+    print ("How are you?")
+
+hi = "hello"
+if hi== "hello":
+    print ("I feel good")
+
+a = 2
+if a == 2:
+    print ("just write me")
+
+a = "k"
+if a== "k":
+    print ("and what?")
