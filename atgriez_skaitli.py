e156e87147d398ef5bc8f306f22e6756023fa5b9
Jaroslav28 <jaroslav030305@gmail.com>
2021-10-12 13:25:34 +0300

    add min_max

---
+++ atgriez_skaitli.py
@@ -0,0 +1,11 @@
+def minimals_un_maksimals(min,skaitlis,max):
+
+    return skaitlis(2,4)
+    print(return_skaitlis(3,5))
+    2 = a
+    3 = b 
+    5 = c 
+    4 = d
+print(max(2,3,5,4))
+print(min(2,3,5,4))
+  
---
+++ linolejs.py
@@ -0,0 +1,9 @@
+def gridas_izmaksas(cena,telpas_garums,telpas_platums):
+
+     
+     cena =10 
+
+     telpas_garums =22
+
+     telpas_platums =18
+
